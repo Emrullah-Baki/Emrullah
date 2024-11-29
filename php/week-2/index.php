0a4226edc9c4ab1a9572b399f9de2655eef9d300
Emrullah-Baki <9024373@student.zadkine.nl>
2024-11-29 13:19:18 +0100

    week2 php

---
+++ php/week-2/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php include 'navbar.php'?>
+
+    <h1>Yokoso sakasama no sekai</h1>
+    <h2>Welcome to the inverted world</h2>
+
+ <?php include 'footer.php'?>
+
+</body>
+</html>
---
+++ php/week-2/navbar.php
@@ -0,0 +1,6 @@
+<nav>
+    <li><a href="Index.php">Home Page</a></li>
+    <li><a href="about.php">About</a></li>
+    <li><a href="contact.php">Contact</a></li>
+
+</nav>
